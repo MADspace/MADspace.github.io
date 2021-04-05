b5fb0531c6d70eb831d7abe14c0949e0a16a3d94
joosthoeks <joosthoeks@gmail.com>
2021-04-05 16:48:56 +0000

    deleted nieuwe structuur

--- docs/pandoc.sh
+++ docs/pandoc.sh
@@ -2,7 +2,6 @@
 pandoc -s -t latex definities.md -o definities.pdf
 pandoc -s -t latex huisregels.md -o huisregels.pdf
 pandoc -s -t latex inventaris.md -o inventaris.pdf
-pandoc -s -t latex Nieuwe_structuur_Stichting_MADspace_Per_1_januari_2021.md -o Nieuwe_structuur_Stichting_MADspace_Per_1_januari_2021.pdf
 pandoc -s -t latex prijslijst.md -o prijslijst.pdf
 pandoc -s -t latex zelf_organiseren.md -o zelf_organiseren.pdf
 
